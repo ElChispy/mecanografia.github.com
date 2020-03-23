542184cf0272804b1269d562e654a215b9e1beea
ElChispy <62524713+ElChispy@users.noreply.github.com>
2020-03-23 02:12:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -46,17 +46,4 @@ async function renderNewQuote() {
   startTimer()
 }
 
-let startTime
-function startTimer() {
-  timerElement.innerText = 0
-  startTime = new Date()
-  setInterval(() => {
-    timer.innerText = getTimerTime()
-  }, 1000)
-}
-
-function getTimerTime() {
-  return Math.floor((new Date() - startTime) / 1000)
-}
-
 renderNewQuote()
